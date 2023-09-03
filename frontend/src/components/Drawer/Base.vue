0b8564a0590bd1962b47004a03fb2c8df0a61563
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2023-09-03 11:33:36 -0400

    Group drawer by generation or family

--- frontend/src/components/Drawer/Base.vue
+++ frontend/src/components/Drawer/Base.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, inject } from "vue";
+import { groupBy, sortBy } from "lodash";
 import storePlatforms from "@/stores/platforms";
 import storeAuth from "@/stores/auth";
 import DrawerHeader from "@/components/Drawer/Header.vue";
@@ -12,6 +13,11 @@ const auth = storeAuth();
 const drawer = ref(undefined);
 const open = ref(["Platforms", "Library", "Settings"]);
 const rail = ref(localStorage.getItem("rail") == "true");
+
+const platformsByGen = groupBy(platforms.value, "generation");
+const platformsByGenKeys = Object.keys(platformsByGen).sort((a, b) => a - b);
+const platformsByFamily = groupBy(platforms.value, "pf_slug");
+const platformsByFamilyKeys = Object.keys(platformsByFamily).sort();
 
 // Event listeners bus
 const emitter = inject("emitter");
@@ -41,7 +47,9 @@ emitter.on("toggleDrawerRail", () => {
       <v-list-group value="Platforms" fluid>
         <template v-slot:activator="{ props }">
           <v-list-item v-bind="props">
-            <span v-if="!rail" class="text-body-1 text-truncate">Platforms</span>
+            <span v-if="!rail" class="text-body-1 text-truncate"
+              >Platforms</span
+            >
             <template v-slot:prepend>
               <v-avatar :rounded="0" size="40"
                 ><v-icon>mdi-controller</v-icon></v-avatar
@@ -49,15 +57,44 @@ emitter.on("toggleDrawerRail", () => {
             </template>
           </v-list-item>
         </template>
+
         <platform-list-item
           v-for="platform in platforms.value"
           :platform="platform"
           :rail="rail"
           :key="platform.slug"
         />
+
+        <template v-for="key in platformsByGenKeys">
+          <span v-if="!rail && key !== '-1'" class="text-body-1 text-truncate">
+            {{ key }}th Generation
+          </span>
+          <platform-list-item
+            v-for="platform in platformsByGen[key]"
+            :platform="platform"
+            :rail="rail"
+            :key="platform.slug"
+          />
+        </template>
+
+        <template v-for="key in platformsByFamilyKeys">
+          <span v-if="!rail" class="text-body-1 text-truncate">
+            {{ platformsByFamily[key][0].pf_name }}
+          </span>
+          <platform-list-item
+            v-for="platform in platformsByFamily[key]"
+            :platform="platform"
+            :rail="rail"
+            :key="platform.slug"
+          />
+        </template>
       </v-list-group>
 
-      <v-list-group value="Library" v-if="auth.scopes.includes('roms.write')" fluid>
+      <v-list-group
+        value="Library"
+        v-if="auth.scopes.includes('roms.write')"
+        fluid
+      >
         <template v-slot:activator="{ props }">
           <v-list-item v-bind="props">
             <span v-if="!rail" class="text-body-1 text-truncate">Library</span>
@@ -78,7 +115,9 @@ emitter.on("toggleDrawerRail", () => {
         </v-list-item>
         <v-list-item class="bg-terciary" disabled>
           <span v-if="!rail" class="text-body-2 text-truncate">Upload</span>
-          <span v-if="!rail" class="text-caption text-truncate ml-1">[coming soon]</span>
+          <span v-if="!rail" class="text-caption text-truncate ml-1"
+            >[coming soon]</span
+          >
           <template v-slot:prepend>
             <v-avatar :rounded="0" size="40"
               ><v-icon>mdi-upload</v-icon></v-avatar
@@ -98,11 +137,10 @@ emitter.on("toggleDrawerRail", () => {
             </template>
           </v-list-item>
         </template>
-        <v-list-item
-          class="bg-terciary"
-          to="/settings/control-panel"
-        >
-          <span v-if="!rail" class="text-body-2 text-truncate">Control Panel</span>
+        <v-list-item class="bg-terciary" to="/settings/control-panel">
+          <span v-if="!rail" class="text-body-2 text-truncate"
+            >Control Panel</span
+          >
           <template v-slot:prepend>
             <v-avatar :rounded="0" size="40"
               ><v-icon>mdi-view-dashboard</v-icon></v-avatar
